4bcb3db61582e544759a03099386d7cea7882580
TaichiYazaki <taichi.yazaki@rakus-partners.co.jp>
2023-02-09 13:53:57 +0900

    userへのお気に入りレストラン質問機能

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from models import robot
 
 #インスタンス化
-restaurant_robot = robot.Robot()
+restaurant_robot = robot.RestaurantRobot()
 restaurant_robot.hello()
+restaurant_robot.ask_user_favorite()
+restaurant_robot
--- models/robot.py
+++ models/robot.py
@@ -10,7 +10,25 @@ class Robot(object):
     def hello(self):
         with open('templates/hello.txt') as template_file:
             file = template_file.read()
-            insert_robot_name =file.replace("$robot_name",self.name)
+            insert_robot_name =file.replace('$robot_name',self.name)
             user_name = input(insert_robot_name)
             self.user_name = user_name.title()
             
+
+class RestaurantRobot(Robot):
+
+    def __init__(self, name=DEFAULT_ROBOT_NAME,):
+        super().__init__(name)
+
+    def decorator(func):
+        def wrapper(*args, **kw):
+            print('デコレーター開始')
+            func(*args, **kw)
+        return wrapper
+
+    @decorator
+    def ask_user_favorite(self):
+        with open('templates/whitch_restaurant.txt') as template_file:
+            file = template_file.read()
+            insert_user_name = file.replace('$user_name', self.user_name)
+            recommend_restaurant = input(insert_user_name)
